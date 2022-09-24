e878a2aef7c9e258d4bf107ea35f92280c05b937
magodo <wztdyl@sina.com>
2022-09-24 12:23:46 +0800

    azurerm_network_interface_application_gateway_backend_address_pool_association

--- internal/resourceset/azure_resource_set_hack.go
+++ internal/resourceset/azure_resource_set_hack.go
@@ -74,7 +74,7 @@ func (rset *AzureResourceSet) populateForVirtualMachine() error {
 		if strings.ToUpper(res.Id.RouteScopeString()) != "/MICROSOFT.COMPUTE/VIRTUALMACHINES" {
 			continue
 		}
-		disks, err := populateManagedResourcesByPath(res, "properties.storageProfile.dataDisks.#.managedDisk.id")
+		disks, err := populateResourceByPath(res, "properties.storageProfile.dataDisks.#.managedDisk.id")
 		if err != nil {
 			return fmt.Errorf(`populating managed disks for %q: %v`, res.Id, err)
 		}
@@ -106,48 +106,112 @@ func (rset *AzureResourceSet) populateForVirtualMachine() error {
 	return nil
 }
 
-// Populate NSG association (if any) for network interfaces
+// Populate following resources for network interfaces if any:
+// - NSG association
+// - Application Gateway Backend Address Pool association
 func (rset *AzureResourceSet) populateForNetworkInterfaces() error {
 	for _, res := range rset.Resources[:] {
 		if strings.ToUpper(res.Id.RouteScopeString()) != "/MICROSOFT.NETWORK/NETWORKINTERFACES" {
 			continue
 		}
-		nsgs, err := populateManagedResourcesByPath(res, "properties.networkSecurityGroup.id")
-		if err != nil {
-			return fmt.Errorf(`populating nsg for %q: %v`, res.Id, err)
-		}
-		if len(nsgs) != 1 {
-			continue
-		}
-		nsg := nsgs[0]
 
-		nicId, err := aztft.QueryId(res.Id.String(), "azurerm_network_interface", false)
+		nsgAssociations, err := networkInterfacePopulateNSGAssociation(res)
 		if err != nil {
-			return fmt.Errorf("querying resource id for %s: %v", res.Id, err)
+			return fmt.Errorf("populating for NIC NSG association: %v", err)
 		}
-		nsgId, err := aztft.QueryId(nsg.Id.String(), "azurerm_network_security_group", false)
+		rset.Resources = append(rset.Resources, nsgAssociations...)
+
+		bapAssociations, err := networkInterfacePopulateApplicationGatewayBackendAddressPoolAssociation(res)
 		if err != nil {
-			return fmt.Errorf("querying resource id for %s: %v", nsg.Id, err)
+			return fmt.Errorf("populating for NIC App Gw BAP association: %v", err)
 		}
-
-		// Create a hypothetic Azure resource id for the association resource: <nic id>/networkSecurityGroups/<nsgName>
-		azureId := res.Id.Clone().(*armid.ScopedResourceId)
-		azureId.AttrTypes = append(azureId.AttrTypes, "networkSecurityGroups")
-		azureId.AttrNames = append(azureId.AttrNames, nsg.Id.(*armid.ScopedResourceId).Names()[0])
-
-		rset.Resources = append(rset.Resources, AzureResource{
-			Id: azureId,
-			PesudoResourceInfo: &PesudoResourceInfo{
-				TFType: "azurerm_network_interface_security_group_association",
-				TFId:   nicId + "|" + nsgId,
-			},
-		})
+		rset.Resources = append(rset.Resources, bapAssociations...)
 	}
 	return nil
 }
 
-// populateManagedResourcesByPath populate the resource ids in the specified paths.
-func populateManagedResourcesByPath(res AzureResource, paths ...string) ([]AzureResource, error) {
+func networkInterfacePopulateNSGAssociation(res AzureResource) ([]AzureResource, error) {
+	nsgs, err := populateResourceByPath(res, "properties.networkSecurityGroup.id")
+	if err != nil {
+		return nil, fmt.Errorf(`populating nsg for %q: %v`, res.Id, err)
+	}
+	if len(nsgs) != 1 {
+		return nil, nil
+	}
+	nsg := nsgs[0]
+
+	tfNicId, err := aztft.QueryId(res.Id.String(), "azurerm_network_interface", false)
+	if err != nil {
+		return nil, fmt.Errorf("querying resource id for %s: %v", res.Id, err)
+	}
+	tfNsgId, err := aztft.QueryId(nsg.Id.String(), "azurerm_network_security_group", false)
+	if err != nil {
+		return nil, fmt.Errorf("querying resource id for %s: %v", nsg.Id, err)
+	}
+
+	// Create a hypothetic Azure resource id for the association resource: <nic id>/networkSecurityGroups/<nsgName>
+	azureId := res.Id.Clone().(*armid.ScopedResourceId)
+	azureId.AttrTypes = append(azureId.AttrTypes, "networkSecurityGroups")
+	azureId.AttrNames = append(azureId.AttrNames, nsg.Id.(*armid.ScopedResourceId).Names()[0])
+
+	return []AzureResource{
+		{
+			Id: azureId,
+			PesudoResourceInfo: &PesudoResourceInfo{
+				TFType: "azurerm_network_interface_security_group_association",
+				TFId:   tfNicId + "|" + tfNsgId,
+			},
+		},
+	}, nil
+}
+
+func networkInterfacePopulateApplicationGatewayBackendAddressPoolAssociation(res AzureResource) ([]AzureResource, error) {
+	var out []AzureResource
+
+	tfNicId, err := aztft.QueryId(res.Id.String(), "azurerm_network_interface", false)
+	if err != nil {
+		return nil, fmt.Errorf("querying resource id for %s: %v", res.Id, err)
+	}
+
+	// First get the ip configurations
+	ipConfigs, err := populateResourceByPath(res, "properties.ipConfigurations.#.id")
+	if err != nil {
+		return nil, fmt.Errorf(`populating ip configs for %q: %v`, res.Id, err)
+	}
+
+	// Iterate each ip config, populate the associated app gw baps
+	for i, ipConfig := range ipConfigs {
+		baps, err := populateResourceByPath(res, fmt.Sprintf("properties.ipConfigurations.%d.properties.applicationGatewayBackendAddressPools.#.id", i))
+		if err != nil {
+			return nil, fmt.Errorf(`populating application gateway backend address pool for the ip config %q of %q: %v`, ipConfig.Id, res.Id, err)
+		}
+
+		for _, bap := range baps {
+			tfAppGwId, err := aztft.QueryId(bap.Id.Parent().String(), "azurerm_application_gateway", false)
+			if err != nil {
+				return nil, fmt.Errorf("querying resource id for %s: %v", bap.Id.Parent(), err)
+			}
+
+			// Create a hypothetic Azure resource id for the association resource: <ip config id>/backendAddressPools/<bap name>
+			azureId := ipConfig.Id.Clone().(*armid.ScopedResourceId)
+			azureId.AttrTypes = append(azureId.AttrTypes, "backendAddressPools")
+			azureId.AttrNames = append(azureId.AttrNames, bap.Id.Names()[1])
+
+			out = append(out, AzureResource{
+				Id: azureId,
+				PesudoResourceInfo: &PesudoResourceInfo{
+					TFType: "azurerm_network_interface_application_gateway_backend_address_pool_association",
+					TFId:   fmt.Sprintf("%s/ipConfigurations/%s|%s/backendAddressPools/%s", tfNicId, ipConfig.Id.Names()[1], tfAppGwId, bap.Id.Names()[1]),
+				},
+			})
+		}
+	}
+
+	return out, nil
+}
+
+// populateResourceByPath populate the resource ids in the specified paths.
+func populateResourceByPath(res AzureResource, paths ...string) ([]AzureResource, error) {
 	b, err := json.Marshal(res.Properties)
 	if err != nil {
 		return nil, fmt.Errorf("marshaling %v: %v", res.Properties, err)
